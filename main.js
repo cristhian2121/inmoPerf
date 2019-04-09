5aa8cd38ae4b5118410264328c496ca7a9b899ca
Cristhian Delgado <ccdelgado@saludelectronica.com>
2019-04-08 23:40:16 -0500

    Primer commit estructura del proyecto, tener en cuenta pasar el html js y css en react

---
+++ main.js
@@ -0,0 +1,43 @@
+// // Get a reference to the database service
+// // var database = firebase.database();
+// // function writeUserData(userId, name, email) {
+// //     firebase.database().ref('accion/' + userId).set({
+// //         username: name,
+// //         email: email
+// //     });
+// // }
+
+// function getAll() {
+//     database.on('accion', function (snapshot) {
+//         console.log(`Los datos son: ${snapshot.val}`);
+//     })
+// }
+
+// function find() {
+
+//     let tipo = document.getElementById('tipoinmueble').value
+//     console.log(tipo)
+
+//     let city = document.getElementById('city').value
+//     // let index = document.getElementById("city").options['selectedIndex']
+
+//     console.log(city);
+//     //console.log(index);
+
+//     let venRent = document.getElementById('saleRent').value
+//     console.log(venRent)
+//     getAll();
+// }
+
+// // var ref = new Firebase("https://juegosdemesa.firebaseio.com/juegos/");
+// // ref.on("child_added", function(snapshot){
+// // 	console.log("El juego actual es ", snapshot.val());
+// // 	console.log("El id actual es ", snapshot.key());
+// // });
+
+
+
+
+
+
+
